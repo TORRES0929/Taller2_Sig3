2ad276517f9ddd7d92969d66b39fa26c022b636e
TORRES0929 <147576603+TORRES0929@users.noreply.github.com>
2023-11-12 14:08:35 -0500

    1.0
    Carga de Geovisor creado mediante qgis2web

---
+++ layer/equipamientos_salud.js
@@ -0,0 +1,18 @@
+var Equipamientos_Salud = 
+{
+"type": "FeatureCollection",
+"name": "salud",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:EPSG::6249" } },
+"features": [
+{ "type": "Feature", "properties": { "nombre": "CENTRO DE SALUD LOS ALAMOS", "servicio": "SALUD", "escala": "LOCAL", "localizacion": "AV 2D N NO 57B N 00" }, "geometry": { "type": "Point", "coordinates": [ 1063282.555404840037227, 877121.059597339946777 ] } },
+{ "type": "Feature", "properties": { "nombre": "FUNDACION CLINICA VERSALLES", "servicio": "SALUD", "escala": "ZONAL", "localizacion": "AV 9 N NO 58-00" }, "geometry": { "type": "Point", "coordinates": [ 1060700.064920279895887, 877308.30966269003693 ] } },
+{ "type": "Feature", "properties": { "nombre": "FUNDACION ALZHAIMER", "servicio": "SALUD", "escala": "LOCAL", "localizacion": "CR 2 OESTE NO 13-71" }, "geometry": { "type": "Point", "coordinates": [ 1058075.017602690029889, 873166.111247460008599 ] } },
+{ "type": "Feature", "properties": { "nombre": "FUNDACION ALZHAIMER ESPECIALIZADO", "servicio": "SALUD", "escala": "LOCAL", "localizacion": "AV 2 O NO 7-90" }, "geometry": { "type": "Point", "coordinates": [ 1058584.281037979992107, 873583.168211190029979 ] } },
+{ "type": "Feature", "properties": { "nombre": "CLINICA SEBASTIAN DE BELALCAZAR", "servicio": "SALUD", "escala": "URBANA", "localizacion": "AV 4 N NO 7N 53" }, "geometry": { "type": "Point", "coordinates": [ 1060056.258845440112054, 873790.230846580001526 ] } },
+{ "type": "Feature", "properties": { "nombre": "CENTRO MEDICO ESPECIALIZADO", "servicio": "SALUD", "escala": "LOCAL", "localizacion": "CL 43 NO 3DN 37" }, "geometry": { "type": "Point", "coordinates": [ 1062014.944966689916328, 876488.609892209991813 ] } },
+{ "type": "Feature", "properties": { "nombre": "SALUD ISC PLUS", "servicio": "SALUD", "escala": "LOCAL", "localizacion": "AV 4 N NO 29N 75" }, "geometry": { "type": "Point", "coordinates": [ 1061947.531790799926966, 877340.905044439947233 ] } },
+{ "type": "Feature", "properties": { "nombre": "CLINICA COMFENALCO", "servicio": "SALUD", "escala": "REGIONAL", "localizacion": "AV 2B CL 23D CL 25" }, "geometry": { "type": "Point", "coordinates": [ 1061441.562569620087743, 875164.792910870048217 ] } },
+{ "type": "Feature", "properties": { "nombre": "DIRECCION DE SANIDAD POLICLINICA", "servicio": "SALUD", "escala": "ZONAL", "localizacion": "AV 10 NO 15 N 29" }, "geometry": { "type": "Point", "coordinates": [ 1060343.690848110010847, 874582.520864229998551 ] } },
+{ "type": "Feature", "properties": { "nombre": "CLINICA DE LOS REMEDIOS", "servicio": "SALUD", "escala": "URBANA", "localizacion": "AV 2 N CL 25 ESQUINA" }, "geometry": { "type": "Point", "coordinates": [ 1061674.175066620111465, 874704.012741649989039 ] } }
+]
+}
